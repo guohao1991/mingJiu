07aa632fc9cc288f01695a89721cef892f507677
jgs <1360375519@qq.com>
2019-05-08 22:37:45 +0800

    jgs

--- wineAgent/js/common.js
+++ wineAgent/js/common.js
@@ -1,14 +1,19 @@
-$(function() {
-	$(".upper-rt-ho").mousemove(function() {
-		$(".upper-nav").css("display", "block");
-	})	
-	$(".upper-rt-ho").mouseout(function(){
-		$(".upper-nav").css("display", "none");
-	})
-	$(".upper-nav").mousemove(function() {
-		$(".upper-nav").css("display", "block");
-	})
-	$(".upper-nav").mouseout(function() {
-		$(".upper-nav").css("display", "none");
-	})
-})
+//搜索框效果
+function hover(){
+	var arr=["y_pro","y_com","y_pinpai","y_baike","y_tjh","y_news"];
+	for(var i=0;i<arr.length;i++){
+		document.getElementById(arr[i]).style="block";
+	}
+}
+function out(){
+	var arr=["y_pro","y_com","y_pinpai","y_baike","y_tjh","y_news"];
+	for(var i=0;i<arr.length;i++){
+		document.getElementById(arr[i]).style="none";
+	}
+}
+function clickXL(y_id,y_type){
+	var arr=["y_pro","y_com","y_pinpai","y_baike","y_tjh","y_news"];
+	for(var i=0;i<arr.length;i++){
+		document.getElementById(arr[i]).style="none";
+	}
+}
