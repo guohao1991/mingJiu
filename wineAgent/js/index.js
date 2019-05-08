7b46644bc258f3d65274310d59ada7542214898e
cuiwkong <cuiwkong@foxmail.com>
2019-05-08 16:58:20 +0800

    cui-1

--- wineAgent/js/index.js
+++ wineAgent/js/index.js
@@ -1,35 +1,3 @@
-//$(function(){
-//	
-//	$(".nav-second-l>li").hover(function(){
-//		$(".nav-second-one").css(""display":"block"")
-//	})
-//	$(".nav-second-one").hover(function(){
-//		$(".nav-second-one").css({"display":"block"})
-//	})
-//})
-/*$(function() {
-	var arry = $(".nav-first>li");
-	console.log(arry)
-	$.each($(".nav-first>li"), function() {
-
-		$(".nav-first>li").mousemove(function() {
-			$(".nav-first>li").children().css("display", "block");
-		})
-		$(".nav-first>li").mouseout(function() {
-			$(".nav-first>li").children().css("display", "none");
-		})
-
-	});
-
-	$(".nav-second-one").mousemove(function() {
-		$(".nav-second-one").css("display", "block");
-	})
-	$(".nav-second-one").mouseout(function() {
-		$(".nav-second-one").css("display", "none");
-	})
-})*/
-
-
 $(function(){
 	$(".nav-first .nav-special").siblings().each(function(){
 		$(this).hover(()=>{
